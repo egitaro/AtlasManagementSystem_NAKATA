8791e503b0ebbcc72f3d81135214ee59965db573
egitaro <moekichi5525@gmail.com>
2022-06-22 13:02:19 +0900

    デバッカー導入

--- AtlasManagementSystem-master/config/app.php
+++ AtlasManagementSystem-master/config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class, //追記
+
     ],
 
     /*
@@ -225,6 +227,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,  //追記
 
     ],
 
