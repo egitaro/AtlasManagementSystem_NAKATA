d8abbc0e5bd99577420267596e40365147e14e7c
egitaro <moekichi5525@gmail.com>
2022-06-26 12:37:33 +0900

    Seeder初期データ登録しました

--- AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
+++ AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 use Illuminate\Database\Seeder;
 use App\Models\Users\User;
@@ -12,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'over_name' => '仲田',
+            'under_name' => '萌木',
+            'over_name_kana' => 'ナカタ',
+            'under_name_kana' => 'モエギ',
+            'mail_address' => 'm@gmail.com',
+            'sex' => '1',
+            'birth_day' => '20000401',
+            'role' => '1',
+            'password' => '0141',
+        ]);
 
     }
 }
